6471bdacefac3ce8f2ffc870fffd69a2d394cc50
moraespaulolucas <moraes.paulolucas@gmail.com>
2022-03-24 20:20:48 -0300

    2 files modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import Navbar from './components/Navbar';
+
 const App = () => {
   return (
+    <>
+      <Navbar />
     <div>
       <h1>helllo world</h1>
     </div>
+    </>
   );
 };
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const styledNavbar = {
+  backgroundColor: 'grey',
+};
+
+const Navbar = () => {
+  return (
+    <nav style={styledNavbar}>
+      <h4>navbar</h4>
+    </nav>
+  );
+};
+
+export default Navbar;
